1c93400da687d323ebd68601c0d8b1a6bed50ec1
GiaHak <giahak@yahoo.com>
2021-08-23 07:51:42 -0400

    buttonclicker

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("load page");
+
+function message() {
+    alert("Ninja was liked!");
+}
+
+
+function login(element) {
+    if(element.innerText == "Login") {
+        element.innerText ="Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(element) {
+    element.remove();
+}
